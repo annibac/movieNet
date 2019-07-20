8e141f3fce85343bdfbcf96ef7ee200bcc2c3ac3
annibal celine <annibal.celine@gmail.com>
2019-07-21 00:49:03 +0200

    first commit : movies model, crud and crudimpl done

---
+++ MovieDataModel/MoviesCRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDataModel
+{
+    public interface MoviesCRUD
+    {
+        List<Movies> FindAll();
+        Movies FindOne(int id);
+        Movies AddOne(String name, String description, String genre);
+        bool DeleteOne(Movies movie);
+        Movies UpdateOne(Movies movie);
+    }
+}
---
+++ MovieNet/MoviesList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+
+namespace MovieNet
+{
+    public class MoviesList : ViewModelBase
+    {
+        public MoviesList()
+        {
+            Title = "Welcome to movies list !";
+            GoToList = new RelayCommand(GoToListExecute, GoToListCanExecute);
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand GoToList { get; } 
+
+        void GoToListExecute()
+        {
+            Title = "Almost there...";
+        }
+
+        bool GoToListCanExecute()
+        {
+            return true;
+        }
+    }
+}
